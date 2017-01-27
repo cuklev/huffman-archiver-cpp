edd89266c13a9bda5933f37beafac4803d609fad
Kristiyan Tsaklev <cuki@cukii.tk>
2017-01-27 04:31:47 +0200

    Enhance cli

--- main.cpp
+++ main.cpp
@@ -12,23 +12,23 @@ const std::string COMPRESS_COMMAND = "c";
 const std::string DECOMPRESS_COMMAND = "d";
 
 int main(int argc, char** argv) {
-	if(argc < 4) {
+	if(argc < 2) {
 		printUsage();
 		return 1;
 	}
 
-	std::ifstream in(argv[2]);
-	std::ofstream out(argv[3]);
+	std::istream* in = argc > 2 ? new std::ifstream(argv[2]) : &std::cin;
+	std::ostream* out = argc > 3 ? new std::ofstream(argv[3]) : &std::cout;
 
 	if(argv[1] == COMPRESS_COMMAND) {
 		std::string data;
-		data.assign(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>());
-		out << compress(data);
+		data.assign(std::istreambuf_iterator<char>(*in), std::istreambuf_iterator<char>());
+		*out << compress(data);
 	}
 	else if(argv[1] == DECOMPRESS_COMMAND) {
 		std::string data;
-		data.assign(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>());
-		out << decompress(data);
+		data.assign(std::istreambuf_iterator<char>(*in), std::istreambuf_iterator<char>());
+		*out << decompress(data);
 	}
 	else {
 		printUsage();

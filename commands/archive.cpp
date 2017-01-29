00da6a62600278a76460042890cfd49f5f079e86
Kristiyan Tsaklev <cuki@cukii.tk>
2017-01-29 04:21:50 +0200

    Implement archiving list of files

--- commands/archive.cpp
+++ commands/archive.cpp
@@ -1,7 +1,30 @@
 #include "archive.hpp"
+#include "../huffman/compress.hpp"
+
+#include<fstream>
+#include<vector>
+
+void archive_files(const std::vector<std::string>& filenames, std::ostream& out_stream) {
+	std::ostreambuf_iterator<char> out_iterator(out_stream);
+
+	for(auto& fn : filenames) {
+		for(char c : fn) *out_iterator = c;
+		*out_iterator = '\0';
+	}
+	*out_iterator = '\0';
+
+	for(auto& fn : filenames) {
+		std::ifstream in(fn);
+		auto freq_table = readTable(in);
+		in.close();
+		in.open(fn);
+		compress(freq_table, in, out_iterator);
+	}
+}
 
 void archive(const char* in_file, const char* out_file) {
-	throw "Not implemented";
+	std::ofstream out(out_file);
+	archive(in_file, out);
 }
 
 void archive(const char* in_file, std::ostream& out_stream) {
@@ -9,5 +32,11 @@ void archive(const char* in_file, std::ostream& out_stream) {
 }
 
 void archive(std::istream& in_stream, std::ostream& out_stream) {
-	throw "Not implemented";
+	std::vector<std::string> filenames;
+	std::string fn;
+
+	while(std::getline(in_stream, fn))
+		filenames.push_back(fn);
+
+	archive_files(filenames, out_stream);
 }
